2e8949962e37e0296443443a7e5b982c79bacc79
dev7ba <dev7ba@protonmail.com>
2021-11-25 12:08:39 +0100

    Added disregarded tx API controller

---
+++ src/main/java/com/mempoolexplorer/txmempool/controllers/api/RepudiatedTxAPIController.java
@@ -0,0 +1,30 @@
+package com.mempoolexplorer.txmempool.controllers.api;
+
+import com.mempoolexplorer.txmempool.entites.AlgorithmType;
+import com.mempoolexplorer.txmempool.entites.RepudiatedTransaction;
+import com.mempoolexplorer.txmempool.repositories.reactive.RepudiatedTxReactiveRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/repudiatedTxAPI")
+public class RepudiatedTxAPIController {
+
+    @Autowired
+    private RepudiatedTxReactiveRepository repudiatedTxReactiveRepository;
+
+    @GetMapping("/repudiatedTxs/{page}/{size}/{algo}")
+    public Flux<RepudiatedTransaction> getIgnoringBlocksby(@PathVariable("page") Integer page,
+            @PathVariable("size") Integer size, @PathVariable("algo") AlgorithmType aType) {
+        return repudiatedTxReactiveRepository.findByaTypeOrderByDbKeyDesc(aType, PageRequest.of(page, size));
+    }
+}
--- src/main/java/com/mempoolexplorer/txmempool/repositories/reactive/RepudiatedTxReactiveRepository.java
+++ src/main/java/com/mempoolexplorer/txmempool/repositories/reactive/RepudiatedTxReactiveRepository.java
@@ -1,11 +1,16 @@
 package com.mempoolexplorer.txmempool.repositories.reactive;
 
+import com.mempoolexplorer.txmempool.entites.AlgorithmType;
 import com.mempoolexplorer.txmempool.entites.RepudiatedTransaction;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface RepudiatedTxReactiveRepository extends ReactiveMongoRepository<RepudiatedTransaction, String> {
 
+    Flux<RepudiatedTransaction> findByaTypeOrderByDbKeyDesc(AlgorithmType aType, Pageable pageable);
 }
